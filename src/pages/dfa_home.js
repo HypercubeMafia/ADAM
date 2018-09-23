dc27717401520be8cbdde14f65f768a9e8fa2ec6
Grant Iraci <grantira@buffalo.edu>
2018-09-23 14:46:39 -0400

    Add Edit Button
    Add an edit button to the toolbar of the machine view page. Updates the
    ToolBar class to allow the insertion of buttons into the toolbar with
    customizable text and onClick action handlers. Also adds a small blank
    space between toolbar title and buttons to clearly distinguish them from
    each other.
    
    Update machine view toolbar to have an edit button. Currently clicking
    the edit button merely informs the user they have clicked it.
    
    Addresses #32.
    
    Signed-off-by: Grant Iraci <grantira@buffalo.edu>

--- src/pages/dfa_home.js
+++ src/pages/dfa_home.js
@@ -5,7 +5,9 @@ import ADAMToolbar from '../components/toolbar';
 class DFAHomePage extends React.Component {
   render() {
     return (
-          <ADAMToolbar title={this.props.title} back={this.props.back} />
+          <ADAMToolbar title={this.props.title} back={this.props.back} btns={[
+            {body : "EDIT", onClick : () => {window.location.href = "https://www.youtube.com/watch?v=90KpM-ojTX4&t=29"}}
+          ]} />
     );
   }
 }
