24e4e8637feb7201b03e9b93653eff1230a27f02
Matt Eichhorn <maeichho@buffalo.edu>
2018-10-01 16:04:31 -0400

    Add canvas to Machine Edit Page. Addresses #44
    Signed-off-by: Matt Eichhorn <maeichho@buffalo.edu>

--- src/pages/machine_edit.js
+++ src/pages/machine_edit.js
@@ -1,14 +1,43 @@
 import React from "react";
 
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 import ADAMToolbar from '../components/toolbar';
+
+const styles = {
+  canvas : {
+    width : "100%",
+    height : 800
+  }
+}
+
+class MachineCanvas extends React.Component {
+
+  render() {
+    return (
+      <canvas style={styles.canvas} />
+    )
+  }
+}
+
 
 class EditPage extends React.Component {
   render() {
     return (
-          <ADAMToolbar title="EDIT" back={this.props.back} />
+    <div>
+      <ADAMToolbar title="EDIT" back={this.props.back} />
+
+      <Paper elevation={1} style={{margin:32, padding:16}}>
+        <MachineCanvas />
+      </Paper>
+    </div>
+
+
+
     );
   }
 }
 
-export default EditPage;
 
+export default EditPage;
