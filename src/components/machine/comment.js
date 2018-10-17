ca7e03b98b3e5849cb51c8d2361f15b10e290383
Kira Ashton <kiraasht@buffalo.edu>
2018-10-17 16:34:42 -0400

    adds box to comments Addresses #75
    Signed-off-by: Kira Ashton <kiraasht@buffalo.edu>
    Signed-off-by: Grant Iraci <grantira@buffalo.edu>

--- src/components/machine/comment.js
+++ src/components/machine/comment.js
@@ -1,42 +1,59 @@
 import React from "react";
-import { Layer, Text } from 'react-konva';
+import { Layer, Text, Group, Rect } from 'react-konva';
 import ReactDOM from 'react-dom';
 
 class Comment extends React.Component {
-  state={ dragBound : pos => {return{x:pos.x,y:pos.y}}}
+  state={ dragBound : pos => {return{x:pos.x,y:pos.y}}, height : 0 }
   width = 250;
   componentDidMount() {
-    
+
     this.setState({dragBound : pos => {
-    const height = ReactDOM.findDOMNode(this).children[0].getHeight();
-    const w = this.props.size.width;
-    const h = this.props.size.height;
-    const x = pos.x < 5 ? 5 : pos.x > w-this.width+5 ? w-this.width+5 : pos.x ;
-    const y = pos.y < 5 ? 5 : pos.y > h-height-5 ? h-height-5 : pos.y ;
-    return{x:x,y:y};
-    }});
+      const height = ReactDOM.findDOMNode(this).children[0].getHeight()
+      const w = this.props.size.width;
+      const h = this.props.size.height;
+      const x = pos.x < 5 ? 5 : pos.x > w-this.width+5 ? w-this.width+5 : pos.x ;
+      const y = pos.y < 5 ? 5 : pos.y > h-this.state.height-5 ? h-this.state.height-5 : pos.y ;
+      return {x : x, y : y};
+    },
+    width : this.textNode.getTextWidth() + 20,
+    height : this.textNode.getHeight(),
+    });
   }
 
-  
-
   render() {
-    
+
+    var commentText = (<Text
+        text = {this.props.comment.com}
+        fontSize = {18}
+        padding = {5}
+        align = {'center'}
+        width={this.width}
+        ref={node => {
+          this.textNode = node;
+        }}
+      />)
+
+    var commentBox = (<Rect
+          width={this.state.width}
+          height={this.state.height}
+          stroke={"lightgray"}
+          strokeWidth={2}
+      />)
+
     return (
       <Layer>
-        
-	<Text
-	 x = {this.props.comment.x}
-         y = {this.props.comment.y}
-	 text = {this.props.comment.com}
-	 fontSize = {18}
-	 padding = {20}
-	 align = {'center'}
-	 draggable
-	 width={this.width}
-         dragBoundFunc = {this.state.dragBound}
-         onDragEnd = {this.props.onDragEnd}
 
-	/>	
+      <Group
+        draggable
+        x={this.props.comment.x}
+        y={this.props.comment.y}
+        dragBoundFunc = {this.dragBound}
+        onDragEnd = {this.props.onDragEnd}
+        onClick = {this.props.onClick}
+      >
+      {commentBox}
+      {commentText}
+      </Group>
       </Layer>
     )
   }
